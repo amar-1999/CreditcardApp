5c6155ac892751c1a607f6fca734ba30171b4460
amar-1999 <77665080+amar-1999@users.noreply.github.com>
2021-01-22 15:01:05 +0530

    Create creditCardDetails.data.ts

---
+++ src/app/fake-db/creditCardDetails.data.ts
@@ -0,0 +1,12 @@
+export class CreditCardDetailsFakeDb {
+  public static creditCardData = [
+    {
+      id: 1,
+      creditCardNumber: '4185 563 9787',
+      cardHolder: 'Harsha Akkineni',
+      expirationDate: '2035-12-20',
+      securityCode: '143',
+      amount: 80000,
+    },
+  ];
+}
